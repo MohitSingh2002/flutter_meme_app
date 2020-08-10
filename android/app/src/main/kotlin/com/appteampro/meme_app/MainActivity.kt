fa9f854805a406b7ae1b9ec63286d0c436cab591
Mohit <mohitsingh11082002@gmail.com>
2020-08-10 21:52:09 +0530

    Initial Commit.

---
+++ android/app/src/main/kotlin/com/appteampro/meme_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appteampro.meme_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            "Hello Flutter!",
+          ),
+        ),
+      ),
+    );
+  }
+}
