a9144ff91a1318ad403ed99d46818f0a714498b7
Mohit <mohitsingh11082002@gmail.com>
2020-08-13 10:54:43 +0530

    Added support of adding infinite number of text in edit meme page dart file.

---
+++ lib/helper/edit_meme_page_helper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class EditMemePageHelper {
+
+  String text;
+  Color color;
+
+  EditMemePageHelper({this.text, this.color});
+
+}
--- lib/pages/edit_meme_page.dart
+++ lib/pages/edit_meme_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:meme_app/helper/edit_meme_page_helper.dart';
 
 class EditMemePage extends StatefulWidget {
 
   final String memeUrl;
   final String memeName;
 
-  const EditMemePage({Key key, this.memeUrl, this.memeName}) : super(key: key);
+  EditMemePage({this.memeUrl, this.memeName});
 
   @override
   _EditMemePageState createState() => _EditMemePageState();
 }
 
 class _EditMemePageState extends State<EditMemePage> {
+
+  List<EditMemePageHelper> list = List<EditMemePageHelper>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +35,8 @@ class _EditMemePageState extends State<EditMemePage> {
         ],
       ),
       body: SafeArea(
-        child: Column(
+        child: Stack(
+//          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Container(
               height: MediaQuery.of(context).size.height*0.7,
@@ -44,18 +49,24 @@ class _EditMemePageState extends State<EditMemePage> {
             ),
             //The below code is just for testing of drag widget.
             //InteractiveViewer is a new widget in latest version of Flutter i.e. 1.20.0.
-            InteractiveViewer(
-              boundaryMargin: EdgeInsets.symmetric(
-                vertical: double.infinity,
-                horizontal: double.infinity,
-              ),
-              child: Text("Hello",style: TextStyle(color: Colors.yellow),),
-            ),
+//            InteractiveViewer(
+//              boundaryMargin: EdgeInsets.symmetric(
+//                vertical: double.infinity,
+//                horizontal: double.infinity,
+//              ),
+//              child: Text("Hello",style: TextStyle(color: Colors.yellow),),
+//            ),
+            getList(),
           ],
         ),
       ),
       bottomNavigationBar: RaisedButton(
-        onPressed: () {},
+        onPressed: () {
+          EditMemePageHelper editMemePageHelper = new EditMemePageHelper(text: "Singh", color: Colors.yellow);
+          list.add(editMemePageHelper);
+          print(list);
+          getList();
+        },
         splashColor: Colors.blue,
         color: Colors.deepPurple,
         child: Text("Add Text"),
@@ -68,4 +79,21 @@ class _EditMemePageState extends State<EditMemePage> {
       ),
     );
   }
+
+  Widget getList() {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 0,),
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: list.length,
+      itemBuilder: (_, index) {
+        return InteractiveViewer(child: Text(list.elementAt(index).text, style: TextStyle(color: list.elementAt(index).color, fontSize: 30,),),
+        boundaryMargin: EdgeInsets.symmetric(
+          vertical: double.infinity,
+          horizontal: double.infinity,
+        ),);
+      },
+    );
+  }
+
 }
